a44bcd1a7a744ad10b6a9a1e4e7ab1adb1bc5902
Rose <31122139+rosepkid@users.noreply.github.com>
2021-09-02 10:07:20 -0500

    duh

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,14 +1,21 @@
-var f1 ;
+let f1, f2;
 
 
 function setup() {
   createCanvas(500, 500);
-  f1 = loadFont("assets/cute.ttf") ;
+  f1 = loadFont("assets/cute.ttf");
+  f2 = loadFont("assets/Bertha.ttf");
+  textAlign(CENTER) ;
 }
 
 function draw() {
-  background("yellow") ;
-  textFont(f1) ;
-  textSize(48) ;
-  text("hello world", 100, 100) ;
+  background("yellow");
+  textFont(f1);
+  textSize(48);
+  text("hello world", width/2, 100);
+
+  textFont(f2);
+  textSize(24) ;
+  text("Go You Redbirds!", width/2, 160) ;
+
 }
