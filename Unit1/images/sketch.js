28964588c902a0a44ef14c218d2acb7b65ca7468
Rose <31122139+rosepkid@users.noreply.github.com>
2021-09-02 11:12:44 -0500

    my images

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,18 +1,16 @@
-let cake, capo, icec;
+let cake, capo, ice;
 
 function setup() {
   createCanvas(500, 500);
   cake = loadImage("assets/cake.jpg");
   capo = loadImage("assets/capo.jpg");
-  icec = loadImage("assets/icec.jpeg");
+  ice = loadImage("assets/icec.jpeg");
   imageMode(CENTER) ;
 }
 
 function draw() {
-  background('green') ;
-
-  image(cake, width/2, height/2, 100, 100);
-  image(capo, width/2, height/2 - 120, 100, 100);
-  image(icec, width/2, height/2 + 120, 100, 100);
-
+  background('green');
+  image(cake, width/2, height/2, 100, 100) ;
+  image(capo, width/2, height/2-120, 100, 100) ;
+  image(ice, width/2, height/2+120, 100, 100) ;
 }
