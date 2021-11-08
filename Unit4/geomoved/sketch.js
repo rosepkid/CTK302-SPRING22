ff1fa44f20c24e5728b8dfc11d3bc2ded0ccd1b3
rosepkid <rose.marshack@gmail.com>
2021-11-08 09:43:17 -0600

    added font and background color to geomoved

--- Unit4/geomoved/sketch.js
+++ Unit4/geomoved/sketch.js
@@ -1,31 +1,39 @@
-var num ;
-var distance ;
+var num;
+var distance;
+var font1;
 
 var locationData;
-function preload(){
 
-    locationData =  getCurrentPosition();
+function preload() {
+  locationData = getCurrentPosition();
+  font1 = loadFont("assets/Mohave-Light.ttf");
 }
 
-function setup(){
-	num = 0 ;
-    intervalCurrentPosition(positionPing, 5000) ;
-	createCanvas(displayWidth, displayHeight) ;
+function setup() {
+  num = 0;
+  textFont(font1, 36);
+  intervalCurrentPosition(positionPing, 5000); // this is what calls positionPing function
+  createCanvas(displayWidth, displayHeight);
+
 }
 
 function draw() {
 
 
 }
-function positionPing(position){
-	textSize(24) ;
-	num++ ;
-	background(255) ;
-    text("lat: " + position.latitude, 10, 40);
-    text("long: " + position.longitude, 10, 90);
-	text("number of updates " + num, 10, 140) ;
 
-	distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi') ;
-	text("you have moved " + distance, 10, 190) ;
+function positionPing(position) {
+  // textSize(36);
+  num++;
+  background("#2452d1");
+  fill("white");
+  text("lat: " + position.latitude, 10, 40);
+  text("long: " + position.longitude, 10, 90);
+  text("number of updates " + num, 10, 140);
+
+  distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi');
+  text("you have moved " + distance, 10, 190);
+
+  text("remember to take a screenshot before you take a picture of your surroundings!", 10, 230);
 
 }
